d29fc6bd2742155bb0a17b4314556b9ee1e14336
devpcmini <devpcmini10@gmail.com>
2023-07-27 00:14:42 +0900

    Initial commit

---
+++ src/main/java/pcm/springpcm/SpringPcmApplication.java
@@ -0,0 +1,13 @@
+package pcm.springpcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pcm/springpcm/SpringPcmApplicationTests.java
@@ -0,0 +1,13 @@
+package pcm.springpcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
