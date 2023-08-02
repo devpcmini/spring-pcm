8faed7c8e9b67fa0a9f4ce287cbe7ff8fcedb27e
devpcmini <devpcmini10@gmail.com>
2023-08-03 00:49:10 +0900

    회원 도메인과 리포지토리 만들기

---
+++ src/main/java/pcm/springpcm/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package pcm.springpcm.repository;
+
+import pcm.springpcm.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll(); //모든 회원 리스트 반환
+}
