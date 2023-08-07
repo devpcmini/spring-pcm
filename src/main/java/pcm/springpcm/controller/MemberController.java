d7de73a07ae91ec26dd86ec0ad87f26fc46268a0
devpcmini <devpcmini10@gmail.com>
2023-08-07 22:48:42 +0900

    회원 웹 기능 - 등록

--- src/main/java/pcm/springpcm/controller/MemberController.java
+++ src/main/java/pcm/springpcm/controller/MemberController.java
@@ -2,6 +2,9 @@ package pcm.springpcm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import pcm.springpcm.domain.Member;
 import pcm.springpcm.service.MemberService;
 
 @Controller
@@ -15,5 +18,19 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
