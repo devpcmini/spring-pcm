ca7468bfd293edebaab73335cbab81eddd2c0d61
devpcmini <devpcmini10@gmail.com>
2023-08-09 23:04:19 +0900

    순수 JDBC 환경 설정 추가 JDBC 리포지토리 구현 개방-폐쇄 원칙(OCP,Open-Closed Principle)  - 확장에는 열려있고, 수정,변경에는 닫혀있다. 스프링의 DI(Dependencies Injection)을 사용하면 기존 코드를 전혀 손대지 않고, 설정만으로 구현 클래스를 변경할 수 있다.

--- src/main/java/pcm/springpcm/SpringConfig.java
+++ src/main/java/pcm/springpcm/SpringConfig.java
@@ -1,13 +1,25 @@
 package pcm.springpcm;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pcm.springpcm.repository.JdbcMemberRepository;
 import pcm.springpcm.repository.MemberRepository;
 import pcm.springpcm.repository.MemoryMemberRepository;
 import pcm.springpcm.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     //스프링 빈에 올려놓기
     @Bean
     public MemberService memberService(){
@@ -16,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
