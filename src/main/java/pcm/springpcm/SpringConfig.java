8ff43ee850f47753e18dbd745d198662eb13ee4b
devpcmini <devpcmini10@gmail.com>
2023-08-15 01:06:30 +0900

    스프링 데이터 JPA

--- src/main/java/pcm/springpcm/SpringConfig.java
+++ src/main/java/pcm/springpcm/SpringConfig.java
@@ -1,13 +1,9 @@
 package pcm.springpcm;
 
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pcm.springpcm.repository.*;
 import pcm.springpcm.service.MemberService;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -19,24 +15,30 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em){
+//        this.em = em;
+//    }
 
-    @Autowired
-    public SpringConfig(EntityManager em){
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     //스프링 빈에 올려놓기
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
