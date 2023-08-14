99c6946660123d394b826fd6fe3cd30a8a0785e4
devpcmini <devpcmini10@gmail.com>
2023-08-14 17:08:01 +0900

    스프링 JdbcTemplate

--- src/main/java/pcm/springpcm/SpringConfig.java
+++ src/main/java/pcm/springpcm/SpringConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pcm.springpcm.repository.JdbcMemberRepository;
+import pcm.springpcm.repository.JdbcTemplateMemberRepository;
 import pcm.springpcm.repository.MemberRepository;
 import pcm.springpcm.repository.MemoryMemberRepository;
 import pcm.springpcm.service.MemberService;
@@ -29,6 +30,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
